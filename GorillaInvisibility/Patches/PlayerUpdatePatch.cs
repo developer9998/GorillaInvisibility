4835bc1aba83c989baa8f27133b1d05e5dfb26a8
dev <81720436+developer9998@users.noreply.github.com>
2024-07-27 19:11:17 -0230

    Update PlayerUpdatePatch.cs

--- GorillaInvisibility/Patches/PlayerUpdatePatch.cs
+++ GorillaInvisibility/Patches/PlayerUpdatePatch.cs
@@ -5,13 +5,23 @@ namespace GorillaInvisibility.Patches
     [HarmonyPatch(typeof(VRRig), "LateUpdate")]
     public class PlayerUpdatePatch
     {
+        private static bool _isHidden;
+
+        public static void Prefix(VRRig __instance)
+        {
+            if (__instance.isOfflineVRRig)
+            {
+                _isHidden = __instance.IsPlayerMeshHidden;
+            }
+        }
+
         public static void Postfix(VRRig __instance)
         {
             if (__instance.isOfflineVRRig)
             {
-                bool canChangeHiddenState = !NetworkSystem.Instance.InRoom || Plugin.IsAllowed;
+                bool canChangeHiddenState = !NetworkSystem.Instance.InRoom || Plugin.IsAllowed || (!Plugin.IsAllowed && _isHidden);
 
-                bool shouldPlayerHide = NetworkSystem.Instance.InRoom;
+                bool shouldPlayerHide = NetworkSystem.Instance.InRoom && Plugin.IsAllowed;
 
                 if (!canChangeHiddenState || __instance.IsPlayerMeshHidden == shouldPlayerHide) return;
 
